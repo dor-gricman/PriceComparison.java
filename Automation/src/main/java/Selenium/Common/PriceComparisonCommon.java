63bc02466fd422bf55aca6732611d9b362c91c28
dor-gricman <135609444+dor-gricman@users.noreply.github.com>
2023-09-12 06:41:31 +0300

    Update PriceComparisonCommon.java

--- Automation/src/main/java/Selenium/Common/PriceComparisonCommon.java
+++ Automation/src/main/java/Selenium/Common/PriceComparisonCommon.java
@@ -10,16 +10,16 @@ import java.util.List;
 
 public class PriceComparisonCommon {
 
-    public static void main(String[] args) throws InterruptedException, SQLException {// This method is used to compare prices of products in different supermarkets
-        List<Products> shoppingCart = Products.generateShoppingCart();// Generate a shopping cart
-        ChromeDriver driver = SeleniumDriver.initializeDriver();// Initialize the driver
-        HaziHinamCommon haziHinam = new HaziHinamCommon(driver);// Create an instance of the HaziHinamCommon class
-        RamiLeviCommon ramiLevi = new RamiLeviCommon(driver);// Create an instance of the RamiLeviCommon class
-        ShufersalCommon shufersal = new ShufersalCommon(driver);// Create an instance of the ShufersalCommon class
-        haziHinam.getProductPrices(shoppingCart);// Get the prices of the products in Hazi Hinam
-        ramiLevi.getProductPrices(shoppingCart);// Get the prices of the products in Rami Levi
-        shufersal.getProductPrices(shoppingCart);// Get the prices of the products in Shufersal
-        ComparisonSummary.Sum(shoppingCart);// Summarize the comparison results
+    public static void main(String[] args) throws InterruptedException, SQLException {
+        List<Products> shoppingCart = Products.generateShoppingCart();
+        ChromeDriver driver = SeleniumDriver.initializeDriver();
+        HaziHinamCommon haziHinam = new HaziHinamCommon(driver);
+        RamiLeviCommon ramiLevi = new RamiLeviCommon(driver);
+        ShufersalCommon shufersal = new ShufersalCommon(driver);
+        haziHinam.getProductPrices(shoppingCart);
+        ramiLevi.getProductPrices(shoppingCart);
+        shufersal.getProductPrices(shoppingCart);
+        ComparisonSummary.Sum(shoppingCart);
         SeleniumDriver.quitDriver(driver);
     }
 }
